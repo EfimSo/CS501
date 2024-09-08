a58dc07bd64d711fcad89b1cabd38537ff92c964
Efim Sokolov <efim@bu.edu>
2024-09-08 01:35:00 -0400

    Button

--- app/src/main/java/com/example/individualassignment1/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment1/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.individualassignment1.ui.theme.IndividualAssignment1Theme
 
 class MainActivity : ComponentActivity() {
@@ -32,11 +44,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    var displayed by remember {
+        mutableStateOf(value = false)
+    }
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (displayed) {
+                Text(
+                    text = "Hello $name!",
+                    fontSize = 30.sp,
+                    modifier = Modifier.height(50.dp)
+                )
+            }
+            else{
+                Spacer(modifier = Modifier.height(50.dp)) // ensures consistent button position
+            }
+            Button(onClick = {
+                displayed = !displayed
+            }) {
+                Text(text = "Click me")
+            }
+        }
+    }
+
+
 
 @Preview(showBackground = true)
 @Composable
